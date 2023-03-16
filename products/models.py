29b31fd584f3ca2dbb6e4201bb6618e2dfb16b41
DESKTOP-5FRD4TM\Lee <leedavidmartina@gmail.com>
2023-03-16 14:48:06 +0100

    Added first model, Products & manager

--- products/models.py
+++ products/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+
+    # Nesting model manager within model makes me nervous
+    class ProductObjects(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(active=True)
+        
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    slug = models.SlugField(max_length=250, unique_for_date='available')
+    produced = models.DateTimeField(default=timezone.now())
+    active = models.BooleanField(default=False)    
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, default=1)
+    producer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='creations')
+    
+    objects = models.Manager()
+    productobjects = ProductObjects()
+
+    class Meta:
+        ordering = ('-produced',)
+    
+    def __str__(self):
+        return self.title
