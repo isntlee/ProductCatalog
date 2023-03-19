db020c45642aeeca7264282d4f5056a4325e9a9d
DESKTOP-5FRD4TM\Lee <leedavidmartina@gmail.com>
2023-03-19 18:35:11 +0100

    Added in api testing

--- products_api/tests.py
+++ products_api/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from products.models import Product, Category
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class ProductTests(APITestCase):
+
+    def test_view_products(self):
+        url = reverse('products_api:listcreate')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.create_product()
+
+    def create_product(self):
+       self.test_category = Category.objects.create(name='django')
+       self.test_user = User.objects.create_user(
+            username='test_user_1', password='123456789')
+       data = {'name':'Product Title', 'producer': 1, 'active': True,
+               'description':'Product description', 'slug':'product-slug'}
+       url = reverse('products_api:listcreate')
+       response = self.client.post(url, data, format='json')
+       self.assertEqual(response.status_code, status.HTTP_201_CREATED)
