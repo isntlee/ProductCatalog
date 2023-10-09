ff397fcc76787971da9fa35dd8676046db185bb6
DESKTOP-5FRD4TM\Lee <leedavidmartina@gmail.com>
2023-10-09 18:58:11 +0200

    Added license

--- products_api/urls.py
+++ products_api/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', ProductList.as_view({'get': 'list', 'post': 'create'})),
     path('<slug:pk>/', ProductList.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
     path('search/', ProductList.as_view({'get': 'retrieve'})),
+
+    # Error-checking these urls at the min
     # path('bread/create', ProductList.as_view({'post': 'create'})),
     # path('bread/product/<slug:pk>/', ProductList.as_view({'get': 'retrieve'})),
     # path('bread/edit/<slug:pk>/', ProductList.as_view({'put': 'update',})),
